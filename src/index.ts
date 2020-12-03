736b91c871560fc940cbbf65555605c25fc5d16b
Denis Zavgorodny <denis@Deniss-MacBook-Pro.local>
2020-12-04 00:17:07 +0200

    got rid of callbacks

--- src/index.ts
+++ src/index.ts
@@ -3,39 +3,37 @@ import status from './status';
 
 function retryMyFetch(http: Fetch, params: decoratorOptions): Fetch {
   let counter = 0;
-  const caller: Fetch = function caller(url: string, options: fetchOptions) {
-    const defaultRefreshCallback: beforeRefetchInterface = () => Promise.resolve(options);
-    const { beforeRefetch = defaultRefreshCallback, maxTryCount = 5 } = params;
-    return new Promise((resolve, reject) => {
-      status.whenWillIdle().then(() => {
-        http(url, options).then((data) => {
-          if (data.ok === true) {
-            resolve(data);
-            return;
-          }
+  const caller: Fetch = async function caller(
+    url: string,
+    options: fetchOptions,
+  ): Promise<Response> {
+    try {
+      const defaultRefreshCallback: beforeRefetchInterface = () => Promise.resolve(options);
+      const { beforeRefetch = defaultRefreshCallback, maxTryCount = 5 } = params;
+      await status.whenWillIdle();
+      status.setBusy();
+      const data = await http(url, options);
 
-          status.setBusy();
-          counter += 1;
-          if (counter > maxTryCount) {
-            status.setIdle();
-            resolve(data);
-            return;
-          }
-          beforeRefetch(url, options, data.status, counter)
-            .then((updatedOptions) => {
-              status.setIdle();
-              return caller(url, updatedOptions).then((resolvedData) => {
-                status.setIdle();
-                resolve(resolvedData);
-              });
-            })
-            .catch((e) => {
-              status.setIdle();
-              reject(e);
-            });
-        });
-      });
-    });
+      if (data.ok === true) {
+        status.setIdle();
+        return data;
+      }
+
+      counter += 1;
+      if (counter > maxTryCount) {
+        status.setIdle();
+        return data;
+      }
+
+      const updatedOptions = await beforeRefetch(url, options, data.status, counter);
+      status.setIdle();
+      const resolvedData = await caller(url, updatedOptions);
+      status.setIdle();
+      return resolvedData;
+    } catch (error) {
+      status.setIdle();
+      throw error;
+    }
   };
   return caller;
 }
