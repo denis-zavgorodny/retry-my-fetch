f20b8f73a1df00fba73f0cff6dd22af613ddd61b
Denis Zavgorodny <denis@Deniss-MacBook-Pro.local>
2020-12-09 22:51:18 +0200

    refactoring

--- src/abort-controller/handle-aborted.ts
+++ src/abort-controller/handle-aborted.ts
@@ -1,9 +1,11 @@
+import { ABORTED_STATUS_CODE, ABORTED_STATUS_TEXT } from '../constants';
+
 const handleAborted = async (error: Error): Promise<Response> => {
-  if (error.name !== 'AbortError') throw error;
+  if (error.name !== ABORTED_STATUS_TEXT) throw error;
 
   return new Response(null, {
-    status: 499,
-    statusText: 'AbortError',
+    status: ABORTED_STATUS_CODE,
+    statusText: ABORTED_STATUS_TEXT,
   });
 };
 
--- src/utils/was-request-rejected.ts
+++ src/utils/was-request-rejected.ts
@@ -1,4 +1,6 @@
+import { ABORTED_STATUS_CODE, ABORTED_STATUS_TEXT } from '../constants';
+
 const wasRequestRejected = (response: Response): boolean =>
-  response.status === 499 && response.statusText === 'AbortError';
+  response.status === ABORTED_STATUS_CODE && response.statusText === ABORTED_STATUS_TEXT;
 
 export default wasRequestRejected;
