5e01f3cd7d7071ec727398573aa550c68993ddac
Denis Zavgorodny <denis.zavgorodny@gmail.com>
2021-03-14 10:41:44 +0200

    Update tests

--- src/counter.spec.js
+++ src/counter.spec.js
@@ -4,18 +4,24 @@ describe('counter functions tests', () => {
   const urlApi = '/api/test/1';
   const urlApi1 = '/api/test/2';
   const urlApi2 = '/api/test/3';
-  it(`after calling increaseCount 2 times for '${urlApi}' getCount should return 2`, () => {
-    increaseCount(urlApi);
-    increaseCount(urlApi);
-    expect(getCount(urlApi)).toEqual(2);
+  describe(`when increaseCount was called 2 times for '${urlApi}'`, () => {
+    it(`it should return 2`, () => {
+      increaseCount(urlApi);
+      increaseCount(urlApi);
+      expect(getCount(urlApi)).toEqual(2);
+    });
   });
-  it(`after calling clearCount for '${urlApi1}' getCount should return 0`, () => {
-    increaseCount(urlApi1);
-    increaseCount(urlApi1);
-    clearCount(urlApi1);
-    expect(getCount(urlApi1)).toEqual(0);
-  });
-  it(`function getCount for not set url - '${urlApi2}' should return 0`, () => {
-    expect(getCount(urlApi2)).toEqual(0);
+  describe(`when clearCount was called for '${urlApi}'`, () => {
+    it(`it should return 0`, () => {
+      increaseCount(urlApi1);
+      increaseCount(urlApi1);
+      clearCount(urlApi1);
+      expect(getCount(urlApi1)).toEqual(0);
+    });  
+  });  
+  describe(`when getCount was called for undefined URL`, () => {
+    it(`it should return 0`, () => {
+      expect(getCount(urlApi2)).toEqual(0);
+    });
   });
 });
